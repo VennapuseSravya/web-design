e57287e98b05acad6067fe0c20927606a6f163a8
sravya <vsry0897@msitprogram.net>
2020-04-10 21:15:50 +0530

    version 1

---
+++ project0/StyleAbout.cs
@@ -0,0 +1,28 @@
+p {
+  color: rgba(77, 69, 23, 0.911);
+}
+
+a {
+  color: rgba(77, 69, 23, 0.911);
+}
+
+div h3, div h4 {
+  color: rgba(77, 69, 23, 0.911);
+}
+div p {
+  color: Black;
+}
+
+.ol2 ol, .ol1 ol, .ol2 ol li, .ol1 ol li {
+  background-color: #f0d7aa;
+}
+
+.ol1 {
+  color: red;
+}
+
+.ol2 {
+  color: green;
+}
+
+/*# sourceMappingURL=StyleAbout.cs.map */
