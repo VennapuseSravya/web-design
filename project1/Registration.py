bf0e84302da224aff98dc1b22af7497571508b91
sravya <vsry0897@msitprogram.net>
2020-04-16 23:54:27 +0530

    pproject1

---
+++ project1/Registration.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+class Registration(db.Model):
+    __tablename__ = "Registration details"
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String,nullable=False)
+    email = db.Column(db.String,nullable=False,primary_key=True)
+    datetime = db.Column(db.String,nullable=False)
--- project1/application.py
+++ project1/application.py
@@ -5,22 +5,28 @@ from flask import Flask, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
+from Registration import *
+from datetime import datetime
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("DATABASE_URL is not set")
 
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
+# # Configure session to use filesystem
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# db.init_app(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Session(app)
+db.init_app(app)
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
-
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
+def _init_(self, timezone=False, fsp=None):
+    super(TIMESTAMP, self)._init_(timezone=timezone)
+    self.fsp = fsp
 @app.route("/register",methods=["GET"])
 def register():
     if request.method == "GET":
@@ -32,8 +38,19 @@ def register():
 
 @app.route("/print",methods=["POST","GET"])
 def display():
-        
+        Registration.query.all()
         f = request.form.get("first-name")
         l = request.form.get("last-name")
-        print(f+" "+l)
-        return render_template("print.html",f=f,l=l)
+        email = request.form.get("email")
+        register = Registration(first_name=f, last_name=l,email=email,datetime=datetime.now())
+        
+        try:
+            db.session.add(register)
+            db.session.commit()
+            print()
+            print(f+" "+l)
+            return render_template("print.html",f=f,l=l)
+        except Exception :
+	        return render_template("error.html", errors = "Details are already given")
+
+
